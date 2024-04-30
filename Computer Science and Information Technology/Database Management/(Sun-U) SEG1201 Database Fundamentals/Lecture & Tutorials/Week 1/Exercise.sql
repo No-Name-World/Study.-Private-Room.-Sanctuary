07afd74a7ac2bd0efab1e55da5aa3c0d8687b0a7
HUDSOn C <159186112+No-Name-World@users.noreply.github.com>
2024-04-30 11:17:55 +0800

    Update Exercise.sql

--- Computer Science and Information Technology/Database Management/(Sun-U) SEG1201 Database Fundamentals/Lecture & Tutorials/Week 1/Exercise.sql
+++ Computer Science and Information Technology/Database Management/(Sun-U) SEG1201 Database Fundamentals/Lecture & Tutorials/Week 1/Exercise.sql
@@ -7,6 +7,16 @@ CREATE TABLE student (
     CGPA NUMBER(*,1)    
 );
 
+desc student
+
 B. 
 INSERT INTO student VALUES ('George','Male','1 Jan 2001','3.2');
 INSERT INTO student VALUES ('Jane','Female','21 Dec 1999','2.6');
+
+C.
+SELECT *
+FROM student;
+
+D.
+DELETE FROM student 
+WHERE student_name = 'George'
