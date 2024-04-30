d527fa79d198308e170322c9d3f93de47bae96a7
HUDSOn C <159186112+No-Name-World@users.noreply.github.com>
2024-04-30 11:09:29 +0800

    Update Exercise.sql

--- Computer Science and Information Technology/Database Management/(Sun-U) SEG1201 Database Fundamentals/Lecture & Tutorials/Week 1/Exercise.sql
+++ Computer Science and Information Technology/Database Management/(Sun-U) SEG1201 Database Fundamentals/Lecture & Tutorials/Week 1/Exercise.sql
@@ -1,1 +1,12 @@
+# Exercise 1
+A.
+CREATE TABLE student (
+    student_name varchar2(20),
+    gender char(6),
+    DOB DATE,
+    CGPA NUMBER(*,1)    
+);
 
+B. 
+INSERT INTO student VALUES ('George','Male','1 Jan 2001','3.2');
+INSERT INTO student VALUES ('Jane','Female','21 Dec 1999','2.6');
